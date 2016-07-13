f450fde9d40e16046053396a1dcec0938241405a
Felipe Cavani <fcavani@gmail.com>
2016-07-13 08:11:59 -0300

    add license header

--- slog_test.go
+++ slog_test.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// license that can be found in the LICENSE file.
+
 package slog_test
 
 import (
